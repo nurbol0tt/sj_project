c856b77c0a50fded22c8dee742d20235f9dc2f38
nurbol0tt <fairtail764@gmail.com>
2023-08-24 13:36:07 +0600

    add endpoint for auth

--- src/routers/main_router.py
+++ src/routers/main_router.py
@@ -1,12 +1,22 @@
-from django.urls import include, path
-
+from django.conf import settings
 from django.contrib import admin
+from django.urls import path, include
+from .yasg_router import urlpatterns as yasg
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('jet/', include('jet.urls', 'jet')),  # Django JET URLS
     path('jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
-    path('api/v1/', include('src.routers.api_router')),
-    path('api/v1/', include('rest_framework.urls')),
+
+    path('api/v1/', include('src.routers.urls')),
+
 ]
+urlpatterns += yasg
+
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- src/routers/urls.py
+++ src/routers/urls.py
@@ -4,9 +4,9 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from src.apps.user import views
 
 urlpatterns = [
-    path("register/", views.RegisterView.as_view()),
+    path('register/', views.RegisterView.as_view()),
     path('token/refresh/', TokenRefreshView.as_view()),
-    path("login/", views.LoginView.as_view()),
+    path('login/', views.LoginView.as_view()),
     path('roles/', views.RoleListView.as_view()),
-    path("me/", views.Profile.as_view()),
+    path('me/', views.Profile.as_view()),
 ]
