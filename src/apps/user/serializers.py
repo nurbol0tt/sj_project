9e5c411b94f171a6ed69660fe7406ccc440bf2f5
nurbol0tt <fairtail764@gmail.com>
2023-08-24 14:22:39 +0600

    add validate for number

--- src/apps/user/serializers.py
+++ src/apps/user/serializers.py
@@ -8,13 +8,23 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         model = User
         fields = ("name", "surname", "phone", "password", "role")
 
+    def validate_phone(self, value):
+        """
+        Validate that the phone number starts with '+996' and is followed by 9 digits.
+        """
+        if not value.startswith("+996"):
+            raise serializers.ValidationError("Phone number must start with '+996'.")
+        if len(value) != 13 or not value[1:].isdigit():
+            raise serializers.ValidationError("Phone number must have 9 digits after '+996'.")
+        return value
+    
     def create(self, validated_data):
         password = validated_data.pop("password", None)
         instance = self.Meta.model(**validated_data)
         instance.set_password(password)
         instance.save()
         return instance
-
+    
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
