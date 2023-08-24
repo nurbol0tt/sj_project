e04bd29258b758878bf131d2e4085f5d3749e074
nurbol0tt <fairtail764@gmail.com>
2023-08-24 14:22:17 +0600

    config db

--- src/config/settings.py
+++ src/config/settings.py
@@ -79,9 +79,13 @@ ASGI_APPLICATION = "src.config.asgi.application" # noqa
 DATABASES_DIR = BASE_DIR.parent
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(DATABASES_DIR, "db.sqlite3"),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
