7e442da0799640b35f90fea4095a602e98947859
nurbol0tt <fairtail764@gmail.com>
2024-01-13 00:18:34 +0600

    change urls

--- src/config/settings.py
+++ src/config/settings.py
@@ -204,14 +204,14 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://139.59.132.105:3000",
-    "https://sj-crm.kg/",
+    "https://sj-crm.kg",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:8000/',
     'http://127.0.0.1:3000/',
     "http://139.59.132.105:3000",
-    "https://sj-crm.kg/",
+    "https://sj-crm.kg",
 ]
 
 CORS_ALLOW_METHODS = [
