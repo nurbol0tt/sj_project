631313c4ffa32b8bc911b1e69391783211a9412e
nurbol0tt <fairtail764@gmail.com>
2024-01-13 00:07:57 +0600

    delete tasks

--- src/apps/patient/tasks.py
+++ src/apps/patient/tasks.py
@@ -4,28 +4,28 @@ from django.utils import timezone
 from .models.patient_models import PatientInfo
 
 
-@shared_task
-def update_patient_status():
-    latest_discharge_dates = (
-        PatientInfo.objects.values('patient')
-        .annotate(
-            latest_discharge=Max(
-                'date_of_discharge'
-            )
-        )
-    )
-
-    patient_status_false = (
-        PatientInfo.objects.filter(
-            date_of_discharge__lte=timezone.now()
-        ).order_by('patient', '-date_of_discharge')
-        .distinct('patient')
-    )
-
-    for patient_info_true in latest_discharge_dates:
-        patient_info_true.patient.in_hospital = True
-        patient_info_true.patient.save()
-
-    for patient_info_false in patient_status_false:
-        patient_info_false.patient.in_hospital = False
-        patient_info_false.patient.save()
+# @shared_task
+# def update_patient_status():
+#     latest_discharge_dates = (
+#         PatientInfo.objects.values('patient')
+#         .annotate(
+#             latest_discharge=Max(
+#                 'date_of_discharge'
+#             )
+#         )
+#     )
+#
+#     patient_status_false = (
+#         PatientInfo.objects.filter(
+#             date_of_discharge__lte=timezone.now()
+#         ).order_by('patient', '-date_of_discharge')
+#         .distinct('patient')
+#     )
+#
+#     for patient_info_true in latest_discharge_dates:
+#         patient_info_true.patient.in_hospital = True
+#         patient_info_true.patient.save()
+#
+#     for patient_info_false in patient_status_false:
+#         patient_info_false.patient.in_hospital = False
+#         patient_info_false.patient.save()
